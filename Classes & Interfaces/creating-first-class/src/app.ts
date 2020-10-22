a88f6a0bbc89daf75c71845197cefece36c8eb6e
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2020-10-22 23:17:14 +0300

    working with access modifiers

--- Classes & Interfaces/creating-first-class/src/app.ts
+++ Classes & Interfaces/creating-first-class/src/app.ts
@@ -1,10 +1,28 @@
 class Department {
-  name: string;
+  public name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
   }
+
+  describe() {
+    console.log("Departament: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
-const accounting = new Department('Accounting..');
-console.log(accounting)
+const accounting = new Department("Accounting..");
+accounting.addEmployee("Gosho");
+accounting.addEmployee("Pesho");
+
+accounting.describe();
+accounting.printEmployeeInformation();
